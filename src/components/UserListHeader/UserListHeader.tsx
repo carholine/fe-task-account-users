b352b3caf584e63b9dbb8bfe3d6215e29ed492ee
carholine <54982514+carholine@users.noreply.github.com>
2024-09-20 01:20:53 +0300

    Create UserListHeader.tsx

---
+++ src/components/UserListHeader/UserListHeader.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Checkbox, { CheckboxProps } from '../Checkbox/Checkbox';
+
+const UserListHeader: React.FC<CheckboxProps> = ({ onChange, checked }) => {
+    return (
+        <div
+            className="
+            user-list-item
+            z-10 top-0 sticky 
+            pb-1
+            text-xs text-c-text-secondary font-medium tracking-[0.035em]
+            bg-white"
+        >
+            <div className="min-w-16 gap-3 flex items-center">
+                <Checkbox checked={checked} onChange={onChange} />
+                <div className="flex-1">User</div>
+            </div>
+            <div className="flex items-center justify-start">Permission</div>
+            <div>{/* Empty title for actions column */}</div>
+        </div>
+    );
+};
+
+export default React.memo(UserListHeader);
